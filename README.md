725468dac037aae65848cdd3b37d31310a1986c5
Manoj S <manojs@cloudera.com>
2017-03-02 13:15:23 +0530

    First commit - working version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hbasesnapshot-wrapper
-Script to take a hbase-snapshot and store its logs
+
+README:
+
+
+
+This script will take a hbase table name as an input and will create a snapshot of it. Logs will be stored to ./logs/runs.txt
+NOTE: The name of the snapshot is currently set to the current epoch time. If you need to take daily snapshots, I have left a commented out line that will change the snapshot name to "snapshot-YYYYMMDD".
+
+Usage:
+bash take_snapshot.sh <hbase_table_name>
---
+++ take_snapshot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Usage:
+# bash take_snapshot.sh table_name
+
+# DISCLAIMER
+# Please note: This script is released for use "AS IS" without any warranties
+# of any kind, including, but not limited to their installation, use, or
+# performance. We disclaim any and all warranties, either express or implied,
+# including but not limited to any warranty of noninfringement,
+# merchantability, and/ or fitness for a particular purpose. We do not warrant
+# that the technology will meet your requirements, that the operation thereof
+# will be uninterrupted or error-free, or that any errors will be corrected.
+#
+# Any use of these scripts and tools is at your own risk. There is no guarantee
+# that they have been through thorough testing in a comparable environment and
+# we are not responsible for any damage or data loss incurred with their use.
+#
+# You are responsible for reviewing and testing any scripts you run thoroughly
+# before use in any non-testing environment.
+
+#export TS=`date +"%Y%m%d"`
+
+export TS=`date "+%s"`
+export WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export LOGS=$WORKING_DIR/logs
+mkdir -p $LOGS
+
+if [ -z $1 ] || [ $1 == '-h' ]
+then
+	echo "Usage: $WORKING_DIR/$0 <table>"
+	exit 1;
+fi
+
+# Log
+echo "New snapshot... table $1:snapshot-$TS" >> $LOGS/runs.txt
+
+echo "snapshot '$1', 'snapshot-$TS'" | /usr/bin/hbase shell -n 2>> $LOGS/runs.txt 
+status=$?
+
+if [ $status -ne 0 ];
+then
+	echo "Snapshot may have failed: $status" >> $LOGS/runs.txt
+fi
+exit $status
